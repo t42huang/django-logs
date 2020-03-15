78077d2f7863eae1e214943c2369097e4d1e2ddf
Tina Huang <t42huang@gmail.com>
2020-03-15 10:16:18 -0400

    avoid one user hacking into access to others' topics and entries

--- djlogs/views.py
+++ djlogs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from django.shortcuts import render, redirect
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -14,7 +15,7 @@ def index(request):
 @login_required
 def topics(request):
     """Show all topics"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'djlogs/topics.html', context)
 
@@ -23,6 +24,9 @@ def topics(request):
 def topic(request, topic_id):
     """Show a specific topic and all related entries"""
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user.
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
 
@@ -74,6 +78,8 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
